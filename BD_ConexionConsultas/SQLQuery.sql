480caf4d6e15222d6b575ccedcd021df32c985c0
jreyes1990 <jarl1990mt@gmail.com>
2022-07-13 13:38:31 -0600

    Se creo 2 procedimientos, 1. consulta paises, 2. cuenta paises

--- BD_ConexionConsultas/SQLQuery.sql
+++ BD_ConexionConsultas/SQLQuery.sql
@@ -1,1 +1,21 @@
-select * from dbo.pais;
+select * from pais;
+
+insert into pais (idpais,nombre) values(5,'EEUU');
+insert into pais (idpais,nombre) values(6,'España');
+
+create procedure uspListarPais
+as
+begin
+	select idpais,nombre 
+	from pais;
+end;
+
+create procedure uspCantidadPais
+as
+begin
+	select count(*) 
+	from pais;
+end;
+
+exec uspListarPais;
+exec uspCantidadPais;
--- BD_SQLServer/ConexionDB.py
+++ BD_SQLServer/ConexionDB.py
@@ -6,10 +6,13 @@ cn = db.connect("Driver={ODBC Driver 17 for SQL Server}; "
                 "Trusted_connection=yes")
 
 cursor = cn.cursor()
-cursor.execute("select * from dbo.pais;")
+#cursor.execute("select * from dbo.pais;")
+cursor.execute("uspListarPais;") #Se reemplazo la consulta (linea 9) por un procedimiento
 
 ruta ="C:\Cursos\PYTHON\Proyecto\BD_ConexionConsultas\\archivoPython.txt"
 file = open(ruta,"w") #Genera archivo .txt
+
+#print(cursor.fetchval()) #Devuelve un valor de consulta
 
 for fila in cursor: #write: sobreescribe en el archivo .txt
     for celda in fila:
