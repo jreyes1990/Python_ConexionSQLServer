512e956ea0bbbf02f64feecc670f54bfa23f2653
jreyes1990 <jarl1990mt@gmail.com>
2022-07-13 13:16:32 -0600

    Generacion y sobreescritura en "archivoPython.txt"

--- BD_SQLServer/ConexionDB.py
+++ BD_SQLServer/ConexionDB.py
@@ -7,7 +7,15 @@ cn = db.connect("Driver={ODBC Driver 17 for SQL Server}; "
 
 cursor = cn.cursor()
 cursor.execute("select * from dbo.pais;")
-for fila in cursor:
+
+ruta ="C:\Cursos\PYTHON\Proyecto\BD_ConexionConsultas\\archivoPython.txt"
+file = open(ruta,"w") #Genera archivo .txt
+
+for fila in cursor: #write: sobreescribe en el archivo .txt
     for celda in fila:
+        file.write(str(celda))
+        file.write("-")
         print(celda)
+    file.write("\n")
     print("_____________________________")
+file.close()
