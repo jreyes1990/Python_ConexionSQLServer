33d07bb15a22b68701c85fbb0eb085c8859822c9
jreyes1990 <jarl1990mt@gmail.com>
2022-07-13 18:06:48 -0600

    Archivos: Configuracion de conexcion BD, descarga diferentes formatos

--- BD_ConexionConsultas/SQLQuery.sql
+++ BD_ConexionConsultas/SQLQuery.sql
@@ -1,7 +1,22 @@
 select * from pais;
+select * from cine;
 
 insert into pais (idpais,nombre) values(5,'EEUU');
 insert into pais (idpais,nombre) values(6,'España');
+
+CREATE TABLE cine (
+id_cine INT PRIMARY KEY NOT NULL,
+nombre VARCHAR(25) NOT NULL,
+id_tipo_cine INT,
+direccion VARCHAR(50),
+fecha_apertura DATETIME,
+b_habilitado INT
+);
+
+insert into cine values(1,'Cine Cas',1,'Paseo Colon','15-03-2021',1);
+insert into cine values(2,'Cine Lark',2,'Paseo Rincon','15-12-2011',1);
+insert into cine values(3,'Cine Rest',3,'Paseo Market','14-05-2012',1);
+insert into cine values(4,'Super Cinemax',1,'En la esquina','09-11-2019',0);
 
 create procedure uspListarPais
 as
--- BD_SQLServer/panda.py
+++ BD_SQLServer/panda.py
@@ -1,14 +1,34 @@
 import pandas as pd
 import pyodbc as db
+import configparser
+import openpyxl
 
-cn = db.connect("Driver={ODBC Driver 17 for SQL Server}; "
-                "server=JREYES1990; "
-                "database=ReservaCine; "
-                "Trusted_connection=yes")
+config = configparser.ConfigParser()
+config.read("ConexionConfig.cs")
+driver = config.get("Cadena","Driver")
+server = config.get("Cadena","Server")
+database = config.get("Cadena","Database")
+trusted = config.get("Cadena","Trusted_Conecc")
 
-df = pd.read_sql_query("select * from pais", cn)
+cn = db.connect("Driver={0}; "
+                "server={1}; "
+                "database={2}; "
+                "Trusted_connection={3}".format(driver,server,database,trusted))
+
+df = pd.read_sql_query("select * from cine", cn)
 
 ruta ="C:\Cursos\PYTHON\Proyecto\BD_ConexionConsultas\\dataframe.txt"
-file = open(ruta,"w") #Genera archivo .txt
+rutaExcel ="C:\Cursos\PYTHON\Proyecto\BD_ConexionConsultas\\dataframe.xlsx"
+rutaHTML ="C:\Cursos\PYTHON\Proyecto\BD_ConexionConsultas\\dataframe.html"
+rutaJSON ="C:\Cursos\PYTHON\Proyecto\BD_ConexionConsultas\\dataframe.json"
+
+#file = open(ruta,"w") #Genera archivo .txt
+#fileExcel = open(rutaExcel,"w") #Genera archivo .txt
+
 print(df)
-file.write(str(df))
+#file.write(str(df))
+#file.close()
+df.to_csv(ruta,index=False)
+df.to_excel(rutaExcel,index=False)
+df.to_html(rutaHTML,index=False)
+df.to_json(rutaJSON,orient="records")
